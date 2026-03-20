fac522a7eaf474d5b3dafc7f69f278837ba6c298
Danny-LM <165749103+Danny-LM@users.noreply.github.com>
2026-03-20 03:25:58 -0600

    feat: implement view placeholders

---
+++ anigura/src/views/OrdersView.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    //
+</script>
+
+<main>
+    <h1>Your Orders</h1>
+</main>
+
---
+++ anigura/src/views/ProfileView.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    //
+</script>
+
+<main>
+    <h1>Your Profile</h1>
+</main>
+
